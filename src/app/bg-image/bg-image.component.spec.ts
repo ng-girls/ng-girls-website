9ffe2e180cd1910711df4595b0ae920a4af638cb
Robert Willemelis <robert.dev@gmx.de>
2021-04-23 18:21:03 +0200

    feat: update angular 10 to 11

--- src/app/bg-image/bg-image.component.spec.ts
+++ src/app/bg-image/bg-image.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BgImageComponent } from './bg-image.component';
 
@@ -6,7 +6,7 @@ describe('BgImageComponent', () => {
   let component: BgImageComponent;
   let fixture: ComponentFixture<BgImageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BgImageComponent ]
     })
