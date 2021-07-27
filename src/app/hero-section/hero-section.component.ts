1dd705fabd80e417186d795fb41ca96510c1c190
Robert Willemelis <robert.dev@gmx.de>
2021-07-27 23:09:07 +0200

    fix: buttons

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -6,7 +6,10 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
     <div id="masthead" class="site-header" role="banner">
       <div>
           <app-bg-image [image]="image"></app-bg-image>
-          <mat-icon class="logo logo-light" [svgIcon]="'logo_vertical'"></mat-icon>
+          <app-logo [logo]="logo">
+          <ng-content></ng-content>
+      </app-logo>
+          
       </div>
     </div>
   `,
@@ -16,19 +19,7 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
     position: relative;
     display: block;
   }
-  .mat-icon{
-    width: 305px;
-    height: 325px;
-    position: absolute;
-    top: 100px;
-    left: 40%;
-  }
-  @media (max-width: 768px){
-    .mat-icon{
-      transform: scale(0.5);
-      left: 10%
-    }
-  }
+  
   
   `]
 })
