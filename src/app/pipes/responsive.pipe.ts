8bc04e8421b201c78206226713d5f9633bb301bf
Robert Willemelis <robert.dev@gmx.de>
2021-08-08 21:57:53 +0200

    chore: refactor responsive pipe

--- src/app/pipes/responsive.pipe.ts
+++ src/app/pipes/responsive.pipe.ts
@@ -5,39 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: true
 })
 export class ResponsivePipe implements PipeTransform {
-  private cachedData: any = null;
-  private cachedUrl = '';
-
   constructor() { }
-  // constructor(private http: HttpClient) { }
 
   transform(url: string, suffix?: string, height?: any): any {
     var ua = navigator.userAgent;
     let isMobile = false;
-    
+    const type = '.webp';
+    suffix = suffix !== '' ? `-${suffix}` : suffix;
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(ua)){
       isMobile = true;
     }
-    let newUrl = url;
-    if(url && url !== '' && suffix && suffix !== ''){
+    if(url && url !== ''){
+      let prefix = '';
       if(height && height !== 700 && isMobile === true){
-        if(url.indexOf('.webp')){
-          newUrl = url.replace('.webp', '-' + height + '-' + suffix + '.webp');
-        }
-      } else {
-
-        if(url.indexOf('.webp')){
-          newUrl = url.replace('.webp', '-' + suffix + '.webp');
-        }
-      }
+        prefix =  '-' + height;
+      } 
+      return  url.replace(type, prefix + suffix + type);
     }
-    // if (url !== this.cachedUrl) {
-    //   this.cachedData = null;
-    //   this.cachedUrl = url;
-    //   this.http.get(url).subscribe(result => this.cachedData = result);
-    // }
-
-    return newUrl;
+    return url;
   }
 
 }
