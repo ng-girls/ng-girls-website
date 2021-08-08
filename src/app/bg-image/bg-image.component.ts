5926842accd2095f649271a7510bfe4c53fd9063
Robert Willemelis <robert.dev@gmx.de>
2021-08-08 22:52:47 +0200

    fix: add different integration of bg-image classes

--- src/app/bg-image/bg-image.component.ts
+++ src/app/bg-image/bg-image.component.ts
@@ -1,4 +1,5 @@
 import { Component,  OnInit, Input } from '@angular/core';
+import { GetDeviceService } from '../service/get-device/get-device.service';
 
 @Component({
   selector: 'app-bg-image',
@@ -15,18 +16,32 @@ export class BgImageComponent implements OnInit {
   loading = 'eager';
   device: any;
   height = 700;
+  extraClasses = '';
 
     
-  constructor() {
+  constructor(getDevice: GetDeviceService,
+     ) {
+      this.device = getDevice.getDevice();
   }
 
   ngOnInit(): void {
-
+// [ngClass]="{'bg-image--responsive': responsive == true,  'bg-image--fadout': fadeout == true}"
     this.loading = this.lazy === true ? 'lazy' : this.loading;
-    this.device = this.image ? this.image.device : null;
     this.height = (this.device && this.device.isMobile) ? this.mobileHeight : 700;
     this.responsive = (this.image && this.image.responsive) ? this.image.responsive : this.responsive;
+    // this.extraClasses = 'bg-image--responsive';
+    if(this.device.isMobile === true){
+      this.extraClasses += ' bg--mobile';
+    }
+    if(this.fadeout){
+      this.extraClasses += ' bg-image--fadout';
+    }
+    
+    
     this.mobileHeight = (this.image && this.image.mobileHeight) ? this.image.mobileHeight : this.mobileHeight;
+  }
+  getClasses(){
+    return this.extraClasses;
   }
 
 }
