37ed011d2e925a2439667f2b608d918a07e42b05
Robert Willemelis <robert.dev@gmx.de>
2022-01-25 13:45:24 +0100

    fix: uncomment console.log

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-setTimeout (
-  function () {
+// setTimeout (
+//   function () {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-  });
+  // });
