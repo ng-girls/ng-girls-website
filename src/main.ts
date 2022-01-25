8d77f1a6f1e056e805651444e44183d2cd3cf92a
Robert Willemelis <robert.dev@gmx.de>
2022-01-25 11:54:39 +0100

    test: settimeout

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+setTimeout (
+  function () {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+  });
