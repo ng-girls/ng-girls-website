fc8b3d29884217076eabeb08b57e35e34f369288
Shmuela <shmuela@ng-girls.org>
2022-08-15 17:27:09 +0300

    add event links

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -6,39 +6,32 @@ import { DomSanitizer } from '@angular/platform-browser';
 const events = [
   {
     id: 'verona-2022',
-    title: 'ngGirls @ AngularDay',
+    title: 'ngGirls @ angularday',
     location: 'Verona',
     city: 'Verona',
     state: 'Italy',
     date: 'October 8',
     year: '2022',
-    announcement: `We're excited to be part of <a href="https://2022.angularday.it/">AngularDay, Verona</a> - and bring the ngGirls workshop to the local community!
-<b>ngGirls @ Verona</b> will take place on Saturday, October 8th, from 9:30-17:30, at the conference venue. For more details and application: `,
-    sponsors: [{link: 'https://2022.angularday.it/', logo: 'assets/angularday-2022/angularday-logo.svg'}]
-  },
-  {
-    id: 'berlin-2022',
-    title: 'ngGirls @ NG-DE',
-    location: 'Berlin',
-    city: 'Berlin',
-    state: 'Germany',
-    date: 'October 8',
-    year: '2022',
-    announcement: `We're excited to be part of <a href="https://2022.angularday.it/">AngularDay, Berlin</a> - and bring the ngGirls workshop to the local community!
-<b>ngGirls @ Verona</b> will take place on Saturday, October 8th, from 9:30-17:30, at the conference venue. For more details and application: `,
-  sponsors: [{link: 'https://2022.angularday.it/', logo: 'assets/angularday-2022/angularday-logo.svg'}]
-  },
-  {
+    applicationForm: 'https://forms.gle/ttLTBJbCFjeA4u8t7',
+    mentorsForm: 'https://forms.gle/YJk8N1g23Ls7jiHH9',
+    announcement: `We're excited to be part of <a href="https://2022.angularday.it/">angularday, Verona</a> - and bring the ngGirls workshop to the local community!
+<b>ngGirls @ angularday</b> will take place on Saturday, October 8th, from 9:30-17:30, at the conference venue. For more details and application: `,
+    sponsors: [{ link: 'https://2022.angularday.it/', logo: 'assets/events/angularday-2022/angularday-logo.svg' }]
+  }, {
     id: 'warsaw-2022',
-    title: 'ngGirls @ NG-Poland',
+    title: 'ngGirls @ ngPoland',
     location: 'Warsaw',
     city: 'Warsaw',
     state: 'Poland',
-    date: 'October 25',
+    date: 'October 23',
     year: '2022',
-    announcement: `We're excited to be part of <a href="https://2022.angularday.it/">AngularDay, Warsaw</a> - and bring the ngGirls workshop to the local community!
-<b>ngGirls @ Verona</b> will take place on Saturday, October 8th, from 9:30-17:30, at the conference venue. For more details and application: `,
-    sponsors: [{link: 'https://2022.angularday.it/', logo: 'assets/angularday-2022/angularday-logo.svg'}]
+    applicationForm: 'https://forms.gle/f9BmEQttGWDYVLJ88',
+    mentorsForm: 'https://forms.gle/co3L9i82Mgx9c25S6',
+    announcement: `<a href="http://ng-poland.pl/" target="_blank">NG-Poland Conf</a> has been hosting ngGirls workshops for many years. 
+Feeling like family, we're happy to come back to Warsaw for the wonderful community, 
+where the participants are enthusiastic and the mentors are so committed. <br/>
+<b>ngGirls @ ngPoland</b> will take place on Sunday, October 23rd, from 10:00-17:00. For more details and application: `,
+    sponsors: [{ link: 'http://ng-poland.pl/', logo: 'assets/events/ngpoland-2022/ngPoland.png' }]
   }
 
 ];
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class TeamComponent implements OnInit {
   team = [
     { id: 'shmuela', name: 'Shmuela Jacobs', image: 'Shmuela.jpg', subtitle: 'Founder, ngGirls'},
-    { id: 'revital', name: 'Revital Friedman', image: 'Revital.jpg', subtitle: 'Founder, ngGirls'},
-    { id: 'robert', name: 'Robert Willemelis', image: 'Robert.jpg', subtitle: 'Founder, ngGirls'},
-    { id: 'martina', name: 'Martina Kraus', image: 'Martina.jpg', subtitle: 'Founder, ngGirls'},
-    { id: 'katarzyna', name: 'Katarzyna Płocka', image: 'Pelcia.jpg', subtitle: 'Founder, ngGirls'},
-    { id: 'alisa', name: 'Alisa Duncan', image: 'Alisa.jpg', subtitle: 'Founder, ngGirls'},
+    { id: 'revital', name: 'Revital Friedman', image: 'Revital.jpg'},
+    { id: 'robert', name: 'Robert Willemelis', image: 'Robert.jpg'},
+    { id: 'martina', name: 'Martina Kraus', image: 'Martina.jpg'},
+    { id: 'katarzyna', name: 'Katarzyna Płocka', image: 'Pelcia.jpg'},
+    { id: 'alisa', name: 'Alisa Duncan', image: 'Alisa.jpg'},
   ];
 
   constructor() {
