5558bb2e125f1eb6aadd0276785f8e975d41dde4
Martina Kraus <martina.kraus@okta.com>
2023-01-30 20:56:16 +0100

    fix typo

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -39,12 +39,12 @@ where the participants are enthusiastic and the mentors are so committed. <br/>
     location: 'Holiday Inn Gent - Expo, an IHG Hotel, Maaltekouter, Gent, Belgien',
     city: 'Ghent',
     state: 'Belgium',
-    date: 'Friday 24',
+    date: 'March 24',
     year: '2023',
     applicationForm: 'https://forms.gle/LjSiygW8nP7gH1kc9',
     mentorsForm: 'https://forms.gle/XYSapXdCzM5GFeix9',
     announcement: `We're excited to be part of <a href="https://ng-be.org/">NG-BE</a> - and bring the ngGirls workshop to the local community!
-<b>ngGirls @ NG-BE</b> will take place on Friday, December 24th, at 9 AM - 5 PM Central European Time (GMT +1) in the Ghent Expo Holiday Inn, Ghent, Belgium. For more details and application: `,
+<b>ngGirls @ NG-BE</b> will take place on Friday, December 24th, from 9 AM - 5 PM Central European Time (GMT +1) in the Ghent Expo Holiday Inn, Ghent, Belgium. For more details and application: `,
     sponsors: [{ link: 'https://ng-be.org/', logo: 'assets/events/ngbe-2023/ng-be-logo.png' }]
   }
 
