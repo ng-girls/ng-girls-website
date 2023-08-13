4618746dbf6b302d1c07b73c78a7bb219a4dd9dc
Shmuela Jacobs <shmool@users.noreply.github.com>
2023-08-13 18:30:55 +0300

    Remove NG-BE and add NG Poland workshop

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -4,23 +4,21 @@ import { map } from 'rxjs/operators';
 
 const events = [
   {
-    id: 'belgium-2023',
-    title: 'ngGirls @ NG-BE',
-    location: 'Holiday Inn Gent - Expo, an IHG Hotel, Maaltekouter, Gent, Belgien',
-    city: 'Ghent',
-    state: 'Belgium',
-    date: 'March 24',
+    id: 'warsaw-2023',
+    title: 'ngGirls @ NG-Poland',
+    location: 'Warsaw',
+    city: 'Warsaw',
+    state: 'Poland',
+    date: 'November 5',
     year: '2023',
-    applicationForm: 'https://forms.gle/LjSiygW8nP7gH1kc9',
-    mentorsForm: 'https://forms.gle/XYSapXdCzM5GFeix9',
-    announcement: `We're excited to be part of <a href="https://ng-be.org/">NG-BE</a> - and bring the ngGirls workshop to the local community!
-<b>ngGirls @ NG-BE</b> will take place on Friday, March 24th, from 9 AM - 5 PM Central European Time (GMT +1) in the Ghent Expo Holiday Inn, Ghent, Belgium. For more details and application: `,
-    sponsors: [
-      { link: 'https://ng-be.org/', logo: 'assets/events/ngbe-2023/ng-be-logo.png' },
-      { link: 'https://ng-be.org/sponsors/peopleware', logo: 'assets/events/ngbe-2023/peopleware.png' },
-    ]
+    applicationForm: '',
+    mentorsForm: '',
+    announcement: `<a href="http://ng-poland.pl/" target="_blank">NG-Poland Conf</a> has been hosting ngGirls workshops for many years.
+Feeling like family, we're happy to come back to Warsaw for the wonderful community,
+where the participants are enthusiastic and the mentors are so committed. <br/>
+<b>ngGirls @ NG-Poland</b> will take place on Sunday, November 5th, from 10:00-17:00. For more details and application: `,
+    sponsors: [{ link: 'http://ng-poland.pl/', logo: 'assets/events/ngpoland-2022/ngPoland.png' }]
   }
-
 ];
 
 @Injectable({
