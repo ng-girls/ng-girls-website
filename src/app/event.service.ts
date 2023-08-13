8d85689a9251c386c84c16ef299ece495f510e0b
Shmuela Jacobs <shmool@users.noreply.github.com>
2023-08-13 18:38:29 +0300

    NG Poland add mentors form

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -5,18 +5,18 @@ import { map } from 'rxjs/operators';
 const events = [
   {
     id: 'warsaw-2023',
-    title: 'ngGirls @ NG-Poland',
+    title: 'ngGirls @ NG Poland',
     location: 'Warsaw',
     city: 'Warsaw',
     state: 'Poland',
     date: 'November 5',
     year: '2023',
     applicationForm: 'https://forms.gle/woPmCW9PVfSzZrZk8',
-    mentorsForm: '',
-    announcement: `<a href="http://ng-poland.pl/" target="_blank">NG-Poland Conf</a> has been hosting ngGirls workshops for many years.
+    mentorsForm: 'https://forms.gle/uYoju5H5iMZhNXxs8',
+    announcement: `<a href="http://ng-poland.pl/" target="_blank">NG Poland Conf</a> has been hosting ngGirls workshops for many years.
 Feeling like family, we're happy to come back to Warsaw for the wonderful community,
 where the participants are enthusiastic and the mentors are so committed. <br/>
-<b>ngGirls @ NG-Poland</b> will take place on Sunday, November 5th, from 10:00-17:00. For more details and application: `,
+<b>ngGirls @ NG Poland</b> will take place on Sunday, November 5th, from 10:00-17:00. For more details and application: `,
     sponsors: [{ link: 'http://ng-poland.pl/', logo: 'assets/events/ngpoland-2022/ngPoland.png' }]
   }
 ];
