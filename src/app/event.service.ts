08a44512472cd329970bb4b0a58f04be95bee2c1
Robert Willemelis <robert.dev@gmx.de>
2024-01-18 00:26:56 +0100

    feat: add jetbrains logo

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -33,7 +33,10 @@ const events: Events = [
     mentorsForm: 'https://docs.google.com/forms/d/e/1FAIpQLSd0sli7Jv9yjRTGq5vspHE_E9HuBO1u2qPlZTJ-0zTTEIbjKw/viewform',
     announcement: `We're excited to be part of <a href="https://2023.ngrome.io/">NG-CONF 2024</a> - and bring the ngGirls workshop to the local community!
     <b>ngGirls @ NG-CONF</b> will take place on Tuesday, March 19th, from 9:00-17:00 in Salt Lake City. For more details and application: `,
-    sponsors: [{ link: 'https://ng-conf.org/', logo: 'assets/events/ngconf-2024/logo.webp' }]
+    sponsors: [
+      { link: 'https://www.jetbrains.com/', logo: 'assets/events/ngconf-2024/jetbrains.svg' },
+      { link: 'https://ng-conf.org/', logo: 'assets/events/ngconf-2024/logo.webp' }
+    ]
   },
   {
     id: 'rome-2024',
@@ -50,6 +53,7 @@ const events: Events = [
     sponsors: [{ link: 'https://ngrome.io/', logo: 'assets/events/rome-2024/logo.svg' }]
   }
 ];
+
 
 @Injectable({
   providedIn: 'root'
